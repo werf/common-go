78db7e2902d8f9356b59442b6837817c0d7fc560
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-04-09 10:07:47 +0300

    refactor(exec): use graceful termination for exec
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/util/exec.go
+++ pkg/util/exec.go
@@ -2,20 +2,14 @@ package util
 
 import (
 	"context"
+	"github.com/werf/common-go/pkg/graceful"
 	"os"
-	"os/exec"
 	"strings"
 )
 
-// ExecWerfBinaryCmd
-// Deprecated: Use ExecWerfBinaryCmdContext instead.
-func ExecWerfBinaryCmd(args ...string) *exec.Cmd {
-	return ExecWerfBinaryCmdContext(context.Background(), args...)
-}
-
 // ExecWerfBinaryCmdContext executes werf binary in a user namespace.
-func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *exec.Cmd {
-	cmd := exec.CommandContext(ctx, strings.TrimSuffix(os.Args[0], "-in-a-user-namespace"), args...)
+func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *graceful.Cmd {
+	cmd := graceful.ExecCommandContext(ctx, strings.TrimSuffix(os.Args[0], "-in-a-user-namespace"), args...)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -23,13 +17,7 @@ func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *exec.Cmd {
 	return cmd
 }
 
-// ExecKubectlCmd
-// Deprecated: Use ExecKubectlContext instead.
-func ExecKubectlCmd(args ...string) *exec.Cmd {
-	return ExecKubectlCmdContext(context.Background(), args...)
-}
-
 // ExecKubectlCmdContext executes werf kubectl command.
-func ExecKubectlCmdContext(ctx context.Context, args ...string) *exec.Cmd {
+func ExecKubectlCmdContext(ctx context.Context, args ...string) *graceful.Cmd {
 	return ExecWerfBinaryCmdContext(ctx, append([]string{"kubectl"}, args...)...)
 }
