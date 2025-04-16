97dbede6f27c2d85bc76947694b0cb8b082e9624
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-04-16 11:00:37 +0300

    refactor(exec): move executor out from pkg/graceful
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/util/exec.go
+++ pkg/util/exec.go
@@ -2,14 +2,14 @@ package util
 
 import (
 	"context"
-	"github.com/werf/common-go/pkg/graceful"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 // ExecWerfBinaryCmdContext executes werf binary in a user namespace.
-func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *graceful.Cmd {
-	cmd := graceful.ExecCommandContext(ctx, strings.TrimSuffix(os.Args[0], "-in-a-user-namespace"), args...)
+func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, strings.TrimSuffix(os.Args[0], "-in-a-user-namespace"), args...)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -18,6 +18,6 @@ func ExecWerfBinaryCmdContext(ctx context.Context, args ...string) *graceful.Cmd
 }
 
 // ExecKubectlCmdContext executes werf kubectl command.
-func ExecKubectlCmdContext(ctx context.Context, args ...string) *graceful.Cmd {
+func ExecKubectlCmdContext(ctx context.Context, args ...string) *exec.Cmd {
 	return ExecWerfBinaryCmdContext(ctx, append([]string{"kubectl"}, args...)...)
 }
